87c66df9942846c8e67e9ca3308618378ebb0c47
Eslam Saeed <eslamsaeed298@gmail.com>
2025-08-19 21:11:23 +0300

    Make Skills slide responsive

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,20 +1,18 @@
 import React from 'react';
 
 export default function Skills({ tool, Title }) {
-    return (
-        <>
-            <h3>{Title}</h3>
-            <div className="skill">
-                <div className="media-scroller">
+    const skillContent = <div className="media-scroller">
                     {tool.map(item => (
                         <img key={item.alt} alt={item.alt} src={item.src} />
                     ))}
                 </div>
-                <div className="media-scroller">
-                    {tool.map(item => (
-                        <img key={item.alt + '-copy'} alt={item.alt} src={item.src} />
-                    ))}
-                </div>
+
+    return (
+        <>
+            <h3>{Title}</h3>
+            <div className="skill">
+                {skillContent}
+                {skillContent}
             </div>
         </>
     );
