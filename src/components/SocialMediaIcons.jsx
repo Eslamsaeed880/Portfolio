9fce4b1f65b6d0156010bee08800cb61ee45c8e9
Eslam Saeed <eslamsaeed298@gmail.com>
2025-08-22 18:02:48 +0300

    Make the icons redirects to my social media accounts

--- src/components/SocialMediaIcons.jsx
+++ src/components/SocialMediaIcons.jsx
@@ -5,8 +5,12 @@ import LINKEDIN_ICON from '../../public/linkedin-icon.svg';
 export default function SocialMediaIcons() {
     return (
       <div className="icons">
-        <img src={GITHUB_ICON} alt="GitHub Icon" />
-        <img src={LINKEDIN_ICON} alt="LinkedIn Icon" />
+        <a href="https://github.com/Eslamsaeed880" target="_blank" rel="noopener noreferrer">
+          <img src={GITHUB_ICON} alt="GitHub Icon" />
+        </a>
+        <a href="https://www.linkedin.com/in/eslam-saeed-359087290/" target="_blank" rel="noopener noreferrer">
+          <img src={LINKEDIN_ICON} alt="LinkedIn Icon" />
+        </a>
       </div>
     );
 }
